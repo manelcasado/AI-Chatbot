4bdd8f69c4c66d136390eac5076b20e12fb95ac0
manel.casado <manel.casadogarrigue@betheluniversity.edu>
2025-05-23 19:00:38 -0400

    Added loading animation, login page & new chat page

--- my-app/app/(admin)/page.tsx
+++ my-app/app/(admin)/page.tsx
@@ -1,7 +1,22 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
-      <main className="">
-        <h1> Let's build this shit!</h1>
+      <main className="p-10 bg-white m-10 rounded-md w-full">
+        <h1 className="text-4xl font-light"> 
+          Welcome to{" "}
+          <span className="text-[#64B5F5] font-semibold">MyAIssistant</span>!
+        </h1>
+        <h2 className="mt-2 mb-10">
+          Your customizable AI Chat Agent that helps you manage your 
+          customer conversations and needs.
+        </h2>
+        <Link href="/create-chatbot">
+          <Button className="bg-[#64B5F5]">
+            Let's create your first chatbot
+          </Button>
+        </Link>
       </main>
   );
 }
---
+++ my-app/lib/startNewChat.ts
@@ -0,0 +1,46 @@
+import client from "@/graphql/apolloClient";
+import { INSERT_CHAT_SESSION, INSERT_GUEST, INSERT_MESSAGE } from "@/graphql/mutations";
+import { gql } from "@apollo/client"
+
+async function startNewChat(
+  guestName: string,
+  guestEmail: string,
+  chatbotId: number
+) {
+  try {
+    // 1. Create a new guest entry
+    const guestResult = await client.mutate({
+      mutation: INSERT_GUEST,
+      variables: { name: guestName, email: guestEmail, created_at: new Date().toISOString()},
+    });
+    const guestId = guestResult.data.insertGuests.id;
+
+    // 2. Initialize a new chat session
+    const chatSessionResult = await client.mutate({
+      mutation: INSERT_CHAT_SESSION,
+      variables: { chatbot_id: chatbotId, guest_id: guestId, created_at: new Date().toISOString() },
+    });
+    const chatSessionId = chatSessionResult.data.insertChat_sessions.id;
+
+    // 3. Insert initial message (optional)
+    await client.mutate({
+      mutation: INSERT_MESSAGE,
+      variables: {
+        chat_session_id: chatSessionId,
+        sender: "ai",
+        created_at: new Date().toISOString(),
+        // TODO: Change this for the dynamic message we want to send in backend
+        content: `Welcome ${guestName}!\n How can I assist you today? :)`,
+      },
+    });
+
+    // TODO: FIX DAISYUI, PLUGIN MISSING !!!
+    console.log("New chat session started succesfully")
+    return chatSessionId;
+
+  } catch (error) {
+    console.error("Error starting new chat session", error)
+  }
+}
+
+export default startNewChat;
